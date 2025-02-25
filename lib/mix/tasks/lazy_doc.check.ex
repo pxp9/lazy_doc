08754c696e97586eda708915999aab3504ba8dd6
pxp9 <pepe.marquezromero@protonmail.com>
2025-02-25 11:05:55 +0200

    fix module not found when LazyDoc check is executed

--- lib/mix/tasks/lazy_doc.check.ex
+++ lib/mix/tasks/lazy_doc.check.ex
@@ -27,6 +27,8 @@ defmodule Mix.Tasks.LazyDoc.Check do
   def run(_command_line_args) do
     _result = LazyDoc.Application.start("", "")
 
+    Mix.Task.run("app.config")
+
     values =
       LazyDoc.extract_data_from_files()
       |> Enum.map(fn entry ->
